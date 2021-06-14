e9087791792b811c7a1fc7fdf7c5679e859dc11c
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-06-14 22:07:07 +0530

    Day 7 Assignment Completed.

---
+++ Day 7/Day7.js
@@ -0,0 +1,67 @@
+//problem 1
+
+/*
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+//printing list of properties in Object
+console.log(Object.getOwnPropertyNames(student));
+
+//printing list of keys in Object
+console.log(Object.keys(student));
+
+//another way to print keys
+var Properties = "";
+for (let keys in student) {
+  Properties += keys + ",";
+}
+console.log(Properties);
+*/
+
+//Problem 2
+
+/*
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+console.log("Before Deletion: ", student, "\n");
+
+delete student.rollno;
+
+console.log("After Deletion: ", student);
+*/
+
+//Problem 3
+
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+const properties = Object.getOwnPropertyNames(student);
+console.log(properties.length);
+
+//Problem 4
+
+/*
+var library = [
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+for (let index in library) {
+  console.log(library[index].readingStatus);
+}
+*/
+
+//Problem 5
+/*
+var Volume = {
+  radius: 3,
+  height: 4,
+};
+
+var Calculation = Math.PI * Volume.radius * Volume.radius * Volume.height;
+var volumeCylinder = Calculation.toFixed(4);
+console.log(volumeCylinder);
+*/
