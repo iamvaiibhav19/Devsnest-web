1b86aa2f07fb674c749f8b434256f512c501b893
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-07-03 18:21:27 +0530

    Day 10 Assignment Completed.

---
+++ Day 10/card.js
@@ -0,0 +1,77 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+// var AB = document.querySelectorAll(".memory-game");
+
+// let counts = document.querySelector(".count");
+// // let available_seats = document.querySelector(".availableseat");
+
+// let x = parseInt(counts.innerHTML);
+// // let y = parseInt(available_seats.innerHTML);
+// for (let i = 0; i < AB.length; i++) {
+//   AB[i].addEventListener("click", () => {
+//     if (AB[i].classList.innerHTML) {
+//       x += 1;
+//     }
+//     counts.innerHTML = x;
+//     // available_seats.innerHTML = y;
+//   });
+// }
